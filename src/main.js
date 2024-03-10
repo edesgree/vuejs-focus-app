628fbf062557112dabef4fd99065a48585424449
Edouard Desgrée <edouard.desgree@gmail.com>
2024-03-10 19:22:22 +0100

    change miragejs server remove developpment env

--- src/main.js
+++ src/main.js
@@ -7,9 +7,8 @@ import { makeServer } from './server.js';
 
 const app = createApp(App);
 
-if (process.env.NODE_ENV === 'development') {
-    makeServer({ environment: 'development' }); // Initialize Mirage server in development environment
-}
+makeServer();
+
 app.use(createPinia());
 
 app.mount('#app');
